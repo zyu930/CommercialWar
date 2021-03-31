c8899e120bd243af33212fcf8d7ffa09e4eb6827
cookfl <op_fmc@sina.com>
2021-03-31 16:53:03 +0800

    1. 重构 GameMain.1 的技能生效部分逻辑，分离技能生效和占领为两个独立状态 2. 新增卡牌一回合只能用一次的判定

--- Assets/TBSGame/Scripts/Cards/Card.cs
+++ Assets/TBSGame/Scripts/Cards/Card.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace TBSGame.Cards
 {
 	public class Card : ScriptableObject
 	{
+        [Title("基础属性")]
 		public GameLink.CardTakeType CardType = GameLink.CardTakeType.None;
 		public int ActionExpend = 1; //行动力消耗
 		public string Title = "一张卡片";
--- Assets/TBSGame/Scripts/Cards/EffectCard.cs
+++ Assets/TBSGame/Scripts/Cards/EffectCard.cs
@@ -12,22 +12,30 @@ namespace TBSGame.Cards
 		public enum EffectType
 		{
 			AddRange,
-			LoseDurable
-		}
+			LoseDurable,
+            Explosion
+        }
 		
 		public enum FlagType
 		{
 			Enemy,
 			Friend
 		}
-		
-		public EffectType effectType = EffectType.AddRange;
+
+        [Title("标准属性")]
+
+        public EffectType effectType = EffectType.AddRange;
 		public FlagType flagType = FlagType.Enemy;
 		
 		public int Value = 0;
 
+        [Title("动态属性")]
         [EnableIf("effectType", EffectType.AddRange)]
         public int ContinuedRound = 0;
+        [DetailedInfoBox("推技能距离...", "发动后，目标格子周围六个方向上的第一格如果另一侧没有节点或争夺目标，则后退一格，否则原地不动。")]
+        [EnableIf("effectType", EffectType.Explosion)]
+        public int PushRange = 0;
+
         public int GetContinuedRound()
 		{
 			int continuedRound = ContinuedRound;

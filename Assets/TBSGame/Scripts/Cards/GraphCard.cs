46bd24942831e3423715a02783f4457b159367e7
cookfl <op_fmc@sina.com>
2021-04-02 09:47:49 +0800

    1. 制作“技能1”图形数据、关联配置文件 2. 赋予玩家“技能1”手牌

--- Assets/TBSGame/Scripts/Cards/GraphCard.cs
+++ Assets/TBSGame/Scripts/Cards/GraphCard.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace TBSGame.Cards
 {
 	[CreateAssetMenu(menuName = ("TBSGame/Cards/Graph Card"))]
 	public class GraphCard : Card
-	{
-		public string SkillName;
+    {
+        public enum EffectType
+        {
+            LoseDurable
+        }
+        public enum FlagType
+        {
+            Both,
+            Enemy,
+            Friend
+        }
+        [Title("标准属性")]
+        public string SkillName;
+        public EffectType effectType = EffectType.LoseDurable;
+        public FlagType flagType = FlagType.Enemy;
+
+        [DetailedInfoBox("影响值...", "对施法者的影响数值。")]
+        public float affectValue = 0f;
+        [DetailedInfoBox("效果值...", "对承受者的影响数值。")]
+        public float effectValue = 0f;
+
 	}
 }

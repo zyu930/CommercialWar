b3dcfba9640aaef05a62a181e17f6421e49ed4aa
cookfl <op_fmc@sina.com>
2021-04-20 20:46:30 +0800

    补给线功能判定增加 1. 补给线被切断以后，被切断的所有节点均受到 1 点耐久伤害 2. 重构节点的伤害机制，增加了 Simple Hit 的自定义伤害入口

---
+++ Assets/TBSGame/Scripts/Custom/SimpleHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace TBSGame.Cards
+{
+	[CreateAssetMenu(menuName = ("TBSGame/Custom/SimpleHit"))]
+	public class SimpleHit : ScriptableObject, IModifyDurable
+	{
+		[DetailedInfoBox("影响值...", "对施法者的影响数值。")]
+		public float affectValue = 0f;
+		[DetailedInfoBox("效果值...", "对承受者的影响数值。")]
+		public float effectValue = 0f;
+		
+		public float GetAffectValue()
+		{
+			return affectValue;
+		}
+		public float GetEffectValue()
+		{
+			return effectValue;
+		}
+	}
+}

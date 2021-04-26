3c5c3b9dae551c714c8624f69596a17463237b06
cookfl <op_fmc@sina.com>
2021-04-26 22:13:00 +0800

    bug 修复 1. 核心不会被打爆判定修复 2. 用户画像技能添加了额外节点奖励

--- Assets/TBSGame/Scripts/Cards/GraphCard.cs
+++ Assets/TBSGame/Scripts/Cards/GraphCard.cs
@@ -18,16 +18,26 @@ namespace TBSGame.Cards
             Enemy,
             Friend
         }
+	    public enum ExtraType
+	    {
+	    	None,
+	    	AddNode
+	    }
         [Title("标准属性")]
 	    public string SkillName;
 	    public string SkillPrefix;
         public EffectType effectType = EffectType.LoseDurable;
-        public FlagType flagType = FlagType.Enemy;
+	    public FlagType flagType = FlagType.Enemy;
+	    public ExtraType extraType = ExtraType.None;
 
         [DetailedInfoBox("影响值...", "对施法者的影响数值。")]
         public float affectValue = 0f;
         [DetailedInfoBox("效果值...", "对承受者的影响数值。")]
-        public float effectValue = 0f;
+	    public float effectValue = 0f;
+	    
+	    [Title("动态属性")]
+	    [EnableIf("extraType", ExtraType.AddNode)]
+	    public int addNodeCount = 0;
 
 	    public float GetAffectValue()
 	    {
@@ -36,6 +46,10 @@ namespace TBSGame.Cards
 	    public float GetEffectValue()
 	    {
 		    return effectValue;
+	    }
+	    public float GetAddNodeCount()
+	    {
+	    	return addNodeCount;
 	    }
 	}
 }

158905530b416aa0d93d4c03c6f5e76cc3303dc2
cookfl <op_fmc@sina.com>
2021-05-16 11:45:28 +0800

    节点牌重构
    1. 代码、架构节点的配置扩展
    2. 代码、架构节点的使用功能

--- Assets/TBSGame/Scripts/Cards/NodeCard.cs
+++ Assets/TBSGame/Scripts/Cards/NodeCard.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace TBSGame.Cards
 {
 	[CreateAssetMenu(menuName = ("TBSGame/Cards/Node Card"))]
 	public class NodeCard : Card
 	{
+		public enum NodeType
+		{
+			Code,
+			Framework
+		}
+		[Title("标准属性")]
 		public int Durable = 1; //耐久值
+		public NodeType NType = NodeType.Code;
 	}
 }

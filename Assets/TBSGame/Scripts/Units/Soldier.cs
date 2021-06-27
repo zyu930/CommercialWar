b0cadcbebc8b0136bac4a517d51bed9b5f0d9059
cookfl <op_fmc@sina.com>
2021-06-27 22:40:10 +0800

    黑白棋机制问题修复

---
+++ Assets/TBSGame/Scripts/Units/Soldier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace TBSGame.Units
+{
+	[CreateAssetMenu(menuName = ("TBSGame/Units/Soldier"))]
+	public class Soldier : Unit
+	{
+		[Title("标准属性")]
+		public int Durable = 1; //耐久值
+		
+		override public GameLink.UnitType UnitType
+		{
+			get {return GameLink.UnitType.Solider;}
+		}
+	}
+}

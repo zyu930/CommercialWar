368e83e8df06f8b2c8e1f1df6fbfe73bb74435b5
cookfl <op_fmc@sina.com>
2021-07-18 11:55:36 +0800

    兵棋功能
    1. 增加兵棋的种类
    2. 增加兵棋外观
    3. 兵棋可以全域生产

--- Assets/TBSGame/Scripts/Units/Soldier.cs
+++ Assets/TBSGame/Scripts/Units/Soldier.cs
@@ -8,8 +8,18 @@ namespace TBSGame.Units
 	[CreateAssetMenu(menuName = ("TBSGame/Units/Soldier"))]
 	public class Soldier : Unit
 	{
+		public enum SoldierType
+		{
+			Simple = 0,
+			Melee = 1,
+			LongRange = 2,
+			Explode = 3,
+			Giant = 4
+		}
+		
 		[Title("标准属性")]
 		public int Durable = 1; //耐久值
+		public SoldierType soldierType = SoldierType.Simple;
 		
 		override public GameLink.UnitType UnitType
 		{

3d41248fbbb93a3f3361860898e6438020b0f0bb
cookfl <op_fmc@sina.com>
2021-08-14 22:13:38 +0800

    更新 AI 寻路
    实现部分基础功能

---
+++ Assets/TBSGame/Scripts/Cards/Units/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace TBSGame.Units
+{
+	[CreateAssetMenu(menuName = ("TBSGame/Units/None Unit"))]
+	public class Unit : ScriptableObject
+	{
+		[Title("基础属性")]
+		public string Title = "单位";
+		public string Description = "我是一个单位";
+		virtual public GameLink.UnitType UnitType
+		{
+			get {return GameLink.UnitType.None;}
+		}
+	}
+}
---
+++ Assets/TBSGame/Scripts/MyUnit.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TbsFramework.Cells;
+using TbsFramework.Pathfinding.Algorithms;
+
+namespace TBSGame
+{
+	public class MyUnit : MonoBehaviour
+	{
+		private static DijkstraPathfinding _pathfinder = new DijkstraPathfinding();
+		
+		Dictionary<Cell, List<Cell>> cachedPaths = null;
+		/// <summary>
+		/// Determines how far on the grid the unit can move.
+		/// </summary>
+		[SerializeField]
+		private float movementPoints;
+		public virtual float MovementPoints
+		{
+			get
+			{
+				return movementPoints;
+			}
+			protected set
+			{
+				movementPoints = value;
+			}
+		}
+		/// <summary>
+		/// Cell that the unit is currently occupying.
+		/// </summary>
+		[SerializeField]
+		[HideInInspector]
+		private Cell cell;
+		public Cell Cell
+		{
+			get
+			{
+				return cell;
+			}
+			set
+			{
+				cell = value;
+			}
+		}
+		/// <summary>
+		/// Method returns all cells that the unit is capable of moving to.
+		/// </summary>
+		public HashSet<Cell> GetAvailableDestinations(List<Cell> cells)
+		{
+			cachedPaths = new Dictionary<Cell, List<Cell>>();
+
+			var paths = CachePaths(cells);
+			foreach (var key in paths.Keys)
+			{
+				if (!IsCellMovableTo(key))
+				{
+					continue;
+				}
+				var path = paths[key];
+
+				var pathCost = path.Sum(c => c.MovementCost);
+				if (pathCost <= MovementPoints)
+				{
+					cachedPaths.Add(key, path);
+				}
+			}
+			return new HashSet<Cell>(cachedPaths.Keys);
+		}
+		private Dictionary<Cell, List<Cell>> CachePaths(List<Cell> cells)
+		{
+			var edges = GetGraphEdges(cells);
+			var paths = _pathfinder.findAllPaths(edges, Cell);
+			return paths;
+		}
+		/// <summary>
+		/// Method returns graph representation of cell grid for pathfinding.
+		/// </summary>
+		protected virtual Dictionary<Cell, Dictionary<Cell, float>> GetGraphEdges(List<Cell> cells)
+		{
+			Dictionary<Cell, Dictionary<Cell, float>> ret = new Dictionary<Cell, Dictionary<Cell, float>>();
+			foreach (var cell in cells)
+			{
+				if (IsCellTraversable(cell) || cell.Equals(Cell))
+				{
+					ret[cell] = new Dictionary<Cell, float>();
+					foreach (Cell neighbour in cell.GetNeighbours(cells).FindAll(IsCellTraversable))
+					{
+						ret[cell][neighbour] = neighbour.MovementCost;
+					}
+				}
+			}
+			return ret;
+		}
+		///<summary>
+		/// Method indicates if unit is capable of moving to cell given as parameter.
+		/// </summary>
+		public virtual bool IsCellMovableTo(Cell cell)
+		{
+			return !cell.IsTaken;
+		}
+		/// <summary>
+		/// Method indicates if unit is capable of moving through cell given as parameter.
+		/// </summary>
+		public virtual bool IsCellTraversable(Cell cell)
+		{
+			return !cell.IsTaken;
+		}
+	}
+}

cec6e753c143dd4b277f906f3ab7cca923b39035
cookfl <op_fmc@sina.com>
2021-08-15 17:19:35 +0800

    更新编辑器
    增加了初始化核心的配置功能

---
+++ Assets/TBSGame/Scripts/Cards/AICoreCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBSGame.Cards
+{
+	[CreateAssetMenu(menuName = ("TBSGame/Cards/AICore Card"))]
+	public class AICoreCard : NodeCard
+	{
+		override public GameLink.CardTakeType CardType
+		{
+			get {return GameLink.CardTakeType.Node;}
+		}
+	}
+}
--- Assets/TBSGame/Scripts/Cards/NodeCard.cs
+++ Assets/TBSGame/Scripts/Cards/NodeCard.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using TBSGame.Units;
 
 namespace TBSGame.Cards
 {
+	[System.Serializable]
+	public struct AICore
+	{
+		public int SpawnTurnCount; //生产周期
+		public int SpawnCount; //生产数量
+		public Soldier solider; //兵棋种类
+	}
+	
 	[CreateAssetMenu(menuName = ("TBSGame/Cards/Node Card"))]
 	public class NodeCard : Card
 	{
@@ -25,8 +34,15 @@ namespace TBSGame.Cards
 		[Title("标准属性")]
 		public int Durable = 1; //耐久值
 		public NodeType NType = NodeType.Code;
-		public ImageType IType = ImageType.None;
 		public int TurnCount = 0; //在场上的时间，0 是不限时间
+		
+		[Title("动态属性")]
+		
+		[EnableIf("@this.NType == NodeType.Code")]
+		public ImageType IType = ImageType.None;
+		
+		[EnableIf("@this.NType == NodeType.Core")]
+		public AICore aiCore;
 		
 		override public GameLink.CardTakeType CardType
 		{

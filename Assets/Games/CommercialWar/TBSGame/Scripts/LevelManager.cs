889d58d17b95140f25672f84a7f7de54c4dea314
cookfl <op_fmc@sina.com>
2021-08-22 16:51:59 +0800

    更新内容
    第一次合并商战到主游戏

---
+++ Assets/Games/CommercialWar/TBSGame/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TBSGame
+{
+	public class LevelManager : MonoBehaviour
+	{
+		public static LevelManager Instance
+		{
+			get; private set;
+		}
+		
+		public UnityEvent OnGameExit;
+		
+		protected void Awake()
+		{
+			if(Instance != null)
+			{
+				Destroy(gameObject);
+			}
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		public void HandlerOnGameExit()
+		{
+			OnGameExit.Invoke();
+		}
+		// 加载商战关卡（Level 1, Level 2 测试两关可选）
+		public void LoadGameLevel(string levelName = "Level 1")
+		{
+			Bolt.CustomEvent.Trigger(gameObject, "LoadGame", levelName);
+		}
+	}
+}
---
+++ Assets/Games/CommercialWar/TBSGame/Scripts/LevelManager_Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TBSGame;
+
+public class LevelManager_Test : MonoBehaviour
+{
+	public void OnGameLevelExit()
+	{
+		Debug.Log("商战退出");
+	}
+	public void LoadGameLeve()
+	{
+		LevelManager.Instance.LoadGameLevel("Level 1");
+	}
+}
